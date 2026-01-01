31dd0d5976ac688087a603d23e72682640703718
vudungeo <vudungeo@gmail.com>
2026-01-01 10:33:11 +0300

    feat: Add Vercel API proxy rewrite and dynamic Vite dev server port configuration.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: true,
-    port: 3000,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     proxy: {
       '/api': {
         target: 'https://raider.io',
